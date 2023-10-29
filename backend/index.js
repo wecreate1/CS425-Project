15e442d16270740c007c508da80ee5d08a1796f3
wecreate1 <41131095+wecreate1@users.noreply.github.com>
2023-10-28 20:17:03 -0500

    Added more read only api

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ app.use(express.json());
 app.route('/api/v1/users')
     .get(async (req, res) => {
         // TODO: assert that authorized user is allowed to do this
-        res.json(await model.User.findAll());
+        const users = await model.User.findAll();
+        res.json(users.map(user => users.forAdmin()));
     });
 // app.route('/api/v1/users/me'); // TODO: when auth is implemented
 app.route('/api/v1/users/:id')
@@ -27,11 +28,15 @@ app.route('/api/v1/users/:id')
             res.sendStatus(404);
             return;
         }
-        res.json(user);
+        if (false /* isAdmin */) {
+            res.json(user.forAdmin());
+        } else {
+            res.json(user.forUser());
+        }
     });
 app.route('/api/v1/courses')
     .get(query('student').optional().isInt(),
-        query('instructor').optional.isInt(),
+        query('instructor').optional().isInt(),
         async (req, res) => {
             if (!validationResult(req).isEmpty()) {
                 res.sendStatus(400);
@@ -45,73 +50,283 @@ app.route('/api/v1/courses')
                 return;
             }
 
+            let courses;
+
             if (student != undefined) {
                 let studentId = parseInt(student);
                 // TODO: assert studentId == authorized user
-                res.json(await model.Course.findManyByStudentId(studentId));
+                courses = await model.Course.findManyByStudentId(studentId);
             } else if (instructor != undefined) {
                 let instructorId = parseInt(instructor);
                 // TODO: assert instructorId == authorized user
-                res.json(await model.Course.findManyByInstructorId(instructorId));
+                courses = await model.Course.findManyByInstructorId(instructorId);
             } else {
                 // TODO: assert that authorized user is allowed to do this
-                res.json(await model.Course.findAll());
+                courses = await model.Course.findAll();
+            }
+
+            if (false /* isAdmin */) {
+                res.json(courses.map(course => course.forAdmin()));
+            } else if (student != undefined) {
+                res.json(courses.map(course => course.forStudent()));
+            } else if (instructor != undefined) {
+                res.json(courses.map(course => course.forInstructor()));
+            } else {
+                res.sendStatus(400);
             }
     });
 app.route('/api/v1/courses/:id')
-    .get(param('id').isInt(), async (req, res) => {
-        if (!validationResult(req).isEmpty()) {
-            res.sendStatus(400);
-            return;
-        }
+    .get(param('id').isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
 
-        let id = parseInt(req.params.id);
+            let id = parseInt(req.params.id);
 
-        // TODO: assert authorized user is enrolled in or instructs course
+            // TODO: assert authorized user is enrolled in or instructs course
 
-        const course = await model.Course.findById(id);
-        if (user == undefined) {
-            res.sendStatus(404);
-            return;
-        }
-        res.json(user);
+            const course = await model.Course.findById(id);
+            if (course == undefined) {
+                res.sendStatus(404);
+                return;
+            }
+            if (false /* isAdmin */) {
+                res.json(course.forAdmin());
+            } else if (req.query.for == 'student') {
+                res.json(course.forStudent());
+            } else if (req.query.for == 'instructor') {
+                res.json(course.forInstructor());
+            } else {
+                res.sendStatus(400);
+            }
     });
 app.route('/api/v1/courses/:id/scale')
-    .get(param('id').isInt(), async (req, res) => {
-        if (!validationResult(req).isEmpty()) {
-            res.sendStatus(400);
-            return;
-        }
-        
-        let id = parseInt(req.params.id);
+    .get(param('id').isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            
+            let id = parseInt(req.params.id);
 
-        // TODO: assert authorized user is enrolled in or instructs course
+            // TODO: assert authorized user is enrolled in or instructs course
 
-        res.json(await model.Scale.findByCourseId(id));
+            const scale = await model.Scale.findByCourseId(id);
+            if (scale == undefined) {
+                res.sendStatus(404);
+                return;
+            }
+            if (false /* isAdmin */) {
+                res.json(scale.forAdmin());
+            } else if (req.query.for == 'student') {
+                res.json(scale.forStudent());
+            } else if (req.query.for == 'instructor') {
+                res.json(scale.forInstructor());
+            } else {
+                res.sendStatus(400);
+            }
     });
 app.route('/api/v1/enrollments')
-    .get((req, res) => {
-        const { course, student } = req.query;
-        // instructors should recieve {id, course, name, email, metadata, [maybe linked status]}
-        // students should recieve {id, course, student, name}
+    .get(query('student').optional().isInt(),
+         query('course').optional().isInt(), async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+
+            const { student,  course } = req.query;
+
+            if (student != undefined && course != undefined) {
+                res.sendStatus(400);
+                return;
+            }
+
+            let enrollments;
+
+            if (student != undefined) {
+                let studentId = parseInt(student);
+                // TODO: assert studentId == authorized user
+                enrollments = await model.Enrollment.findManyByStudentId(studentId);
+            } else if (course != undefined) {
+                let courseId = parseInt(course);
+                // TODO: assert authorized user is instructor of course
+                enrollments = await model.Enrollment.findManyByCourseId(courseId);
+            } else {
+                // TODO: assert that authorized user is allowed to do this
+                enrollments = await model.Enrollment.findAll();
+            }
+            if (false /* isAdmin */) {
+                res.json(enrollments.map(enrollment => enrollment.forAdmin()));
+            } else if (student != undefined) {
+                res.json(enrollments.map(enrollment => enrollment.forStudent()));
+            } else if (course != undefined) {
+                res.json(enrollments.map(enrollment => enrollment.forInstructor()));
+            } else {
+                res.sendStatus(400);
+            }
     });
-app.route('/api/v1/enrollments/:id');
+app.route('/api/v1/enrollments/:id')
+    .get(param('id').isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            const id = parseInt(req.params.id);
+
+            // TODO: assert authorized user is enrolled in or instructs course
+
+            const enrollment = await model.Enrollment.findById(id);
+            if (enrollment == undefined) {
+                res.sendStatus(404);
+                return;
+            }
+            if (false /* isAdmin */) {
+                res.json(enrollment.forAdmin());
+            } else if (req.query.for == 'student') {
+                res.json(enrollment.forStudent());
+            } else if (req.query.for == 'instructor') {
+                res.json(enrollment.forInstructor());
+            } else {
+                res.sendStatus(400);
+            }
+         });
 app.route('/api/v1/instructs')
-    .get((req, res) => {
-        const { instructor } = req.query;
-        // TODO: support multiple instructors on a course
+    .get(query('instructor').isInt(),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            const { instructor } = req.query;
+            const instructorId = parseInt(instructor);
+            // TODO: auth
+            const instructss = await model.Instructs.findManyByInstructorId(instructorId);
+            if (false /* isAdmin */) {
+                res.json(instructss.map(instructs => instructs.forAdmin()));
+            } else {
+                res.json(instructss.map(instructs => instructs.forInstructor()))
+            }
     });
-app.route('/api/v1/instructs/:id');
 app.route('/api/v1/weights')
-    .get((req, res) => {
-        const { course } = req.query;
+    .get(query('course').optional().isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            const { course } = req.query;
+
+            // TODO: auth
+
+            let weights;
+
+            if (course != undefined) {
+                const courseId = parseInt(course);
+                weights = await model.Weight.findManyByCourseId(courseId);
+            } else {
+                weights = await model.Weight.findAll();
+            }
+
+            if (false /* isAdmin */) {
+                res.json(weights.map(weight => weight.forAdmin()));
+            } if (req.query.for == 'student') {
+                res.json(weights.map(weight => weight.forStudent()));
+            } else if (req.query.for == 'instructor') {
+                res.json(weights.map(weight => weight.forInstructor()));
+            } else {
+                res.sendStatus(400);
+            }
+
     });
-app.route('/api/v1/weights/:id');
+app.route('/api/v1/weights/:id')
+    .get(param('id').isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            const id = parseInt(req.params.id);
+
+            // TODO: auth
+
+            const weight = await model.Weight.findById(id);
+            if (weight == undefined) {
+                res.sendStatus(404);
+                return;
+            }
+            if (false /* isAdmin */) {
+                res.json(weight.forAdmin())
+            } else if (req.query.for == 'student') {
+                res.json(weight.forStudent());
+            } else if (req.query.for == 'instructor') {
+                res.json(weight.forInstructor());
+            } else {
+                res.sendStatus(400);
+            }
+         });
 app.route('/api/v1/assignments')
-    .get((req, res) => {
-        const { weight, course } = req.query;
+    .get(query('weight').optional().isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+            const { weight } = req.query;
+            // TODO: auth
+            let assignments;
+            if (weight != undefined) {
+                const weightId = parseInt(weight);
+                assignments = await model.Assignment.findByWeight(weightId);
+            } else {
+                assignments = await model.Assignment.findAll();
+            }
+
+            if (false /* isAdmin */) {
+                res.json(assignments.map(assignment => assignment.forAdmin()));
+            } else if (req.query.for == 'student') {
+                res.json(assignments.map(assignment => assignment.forStudent()));
+            } else if (req.query.for == 'instructor') {
+                res.json(assignments.map(assignment => assignment.forInstructor()));
+            } else {
+                res.sendStatus(400);
+            }
     });
-app.route('/api/v1/assignments/:id');
+app.route('/api/v1/assignments/:id')
+    .get(param('id').isInt(),
+         query('for').isIn(['student', 'instructor', 'admin']),
+         async (req, res) => {
+            if (!validationResult(req).isEmpty()) {
+                res.sendStatus(400);
+                return;
+            }
+
+            const id = parseInt(req.params.id);
+            const assignment = model.Assignment.findById(id);
+            // TODO: auth
+            if (assignment == undefined) {
+                res.sendStatus(404);
+                return;
+            }
+            if (false /* isAdmin */) {
+                res.json(assignment.forAdmin())
+            } else if (req.query.for == 'student') {
+                res.json(assignment.forStudent());
+            } else if (req.query.for == 'instructor') {
+                res.json(assignment.forInstructor());
+            } else {
+                res.sendStatus(400);
+            }
+        });
 app.route('/api/v1/evaluations')
     .get((req, res) => {
         const { assignment, weight, course, enrollee, student, instructor } = req.query;
--- backend/model/index.js
+++ backend/model/index.js
@@ -27,7 +27,7 @@ export class User {
         return result.rows.map((obj) => new User(obj));
     }
 
-    toJSON() {
+    forUser() {
         return {id: this.id, name: this.name};
     }
 }
@@ -78,7 +78,15 @@ export class Course {
         return result.rows.map((obj) => new Course(obj));
     }
 
-    toJSON() {
+    forStudent() {
+        return {id: this.id, name: this.name, credits: this.credits};
+    }
+
+    forInstructor() {
+        return {id: this.id, name: this.name, credits: this.credits};
+    }
+
+    forAdmin() {
         return {id: this.id, name: this.name, credits: this.credits};
     }
 }
@@ -102,7 +110,210 @@ export class Scale {
         return new Scale(result.rows);
     }
 
-    toJSON() {
+    forStudent() {
         return {marks: this.marks};
+    }
+
+    forInstructor() {
+        return {marks: this.marks};
+    }
+
+    forAdmin() {
+        return {marks: this.marks};
+    }
+}
+
+export class Enrollment {
+    constructor(obj) {
+        ({id: this.id, course: this.course, student: this.student, name: this.name, email: this.email, metadata: this.metadata} = obj);
+    }
+
+    get isLinked() {
+        return this.student != null;
+    }
+
+    static async findById(id) {
+        const result = await db.query(sql`
+            SELECT Enrollments.id, Enrollments.course, Enrollments.student, Enrollments.name, Enrollments.email, Enrollments.metadata
+            FROM Enrollments
+            WHERE id=${id};
+        `);
+
+        if (result.rows.length == 1) {
+            return new Course(result.rows[0]);
+        }
+    }
+
+    static async findManyByStudentId(studentId) {
+        const result = await db.query(sql`
+            SELECT Enrollments.id, Enrollments.course, Enrollments.student, Enrollments.name, Enrollments.email, Enrollments.metadata
+            FROM Enrollments
+            WHERE student=${studentId};
+        `);
+    
+        return result.rows.map((obj) => new Enrollment(obj));
+    }
+
+    static async findManyByCourseId(courseId) {
+        const result = await db.query(sql`
+            SELECT Enrollments.id, Enrollments.course, Enrollments.student, Enrollments.name, Enrollments.email, Enrollments.metadata
+            FROM Enrollments
+            WHERE course=${courseId};
+        `);
+    
+        return result.rows.map((obj) => new Enrollment(obj));
+    }
+
+    static async findAll(id) {
+        const result = await db.query(sql`
+            SELECT Enrollments.id, Enrollments.course, Enrollments.student, Enrollments.name, Enrollments.email, Enrollments.metadata
+            FROM Enrollments;
+        `);
+
+        return result.rows.map((obj) => new Course(obj));
+    }
+
+    forStudent() {
+        return {id: this.id, course: this.course, student: this.student, name: this.name};
+    }
+
+    forInstructor() {
+        return {id: this.id, course: this.course, isLinked: this.isLinked, name: this.name, email: this.email, metadata: this.metadata};
+    }
+
+    forAdmin() {
+        return {id: this.id, course: this.course, student: this.student, isLinked: this.isLinked, name: this.name, email: this.email, metadata: this.metadata};
+    }
+}
+
+export class Instructs {
+    constructor(obj) {
+        ({coure: this.course, instructor: this.instructor} = obj);
+    }
+
+    static async findManyByCourseId(courseId) { // just in case we end up supporting multiple instructors per course
+        const result = await db.query(sql`
+            SELECT course, instructor
+            FROM Instructs
+            WHERE course=${courseId};
+        `);
+
+        return result.rows.map(obj => new Instructs(obj));
+    }
+
+    static async findManyByInstructorId(instructorId) {
+        const result = await db.query(sql`
+            SELECT course, instructor
+            FROM Instructs
+            WHERE instructor=${instructorId};
+        `);
+
+        return result.rows.map(obj => new Instructs(obj));
+    }
+
+    forInstructor() {
+        return {course: this.course, instructor: this.instructor};
+    }
+
+    forAdmin() {
+        return {course: this.course, instructor: this.instructor};
+    }
+}
+
+export class Weight {
+    constructor(obj) {
+        ({id: this.id, course: this.coure, name: this.name, weight: this.weight, expected_max_score: this.expected_max_score, drop_n: this.drop_n} = obj);
+    }
+
+    static async findById(id) {
+        const result = await db.query(sql`
+            SELECT id, course, name, weight, expected_max_score, drop_n
+            FROM Weights
+            WHERE id=${id};
+        `);
+
+        if (result.rows.length == 1) {
+            return new Weight(result.rows.length[0]);
+        }
+    }
+
+    static async findManyByCourseId(courseId) {
+        const result = await db.query(sql`
+            SELECT id, course, name, weight, expected_max_score, drop_n
+            FROM Weights
+            WHERE course=${courseId};
+        `);
+
+        return result.rows.map(obj => new Weight(obj));
+    }
+
+    static async findAll() {
+        const result = await db.query(sql`
+            SELECT id, course, name, weight, expected_max_score, drop_n
+            FROM Weights;
+        `);
+
+        return result.rows.map(obj => new Weight(obj));
+    }
+
+    forStudent() {
+        return {id: this.id, course: this.id, name: this.name, weight: this.weight, expected_max_score: this.expected_max_score, drop_n: this.drop_n};
+    }
+
+    forInstructor() {
+        return {id: this.id, course: this.id, name: this.name, weight: this.weight, expected_max_score: this.expected_max_score, drop_n: this.drop_n};
+    }
+
+    forAdmin() {
+        return {id: this.id, course: this.id, name: this.name, weight: this.weight, expected_max_score: this.expected_max_score, drop_n: this.drop_n};
+    }
+}
+
+export class Assignment {
+    constructor(obj) {
+        ({id: this.id, weight: this.weight, name: this.name, max_score: this.max_score} = obj);
+    }
+
+    static async findById(id) {
+        const result = await db.query(sql`
+            SELECT id, weight, name, max_score
+            FROM Assignments
+            WHERE id = ${id};
+        `);
+
+        if (result.rows.length == 1) {
+            return new Assignment(result.rows[0]);
+        }
+    }
+
+    static async findByWeight(id) {
+        const result = await db.query(sql`
+            SELECT id, weight, name, max_score
+            FROM Assignments
+            WHERE weight = ${id};
+        `);
+
+        return result.rows.map(obj => new Assignment(obj));
+    }
+
+    static async findAll(id) {
+        const result = await db.query(sql`
+            SELECT id, weight, name, max_score
+            FROM Assignments;
+        `);
+
+        return result.rows.map(obj => new Assignment(obj));
+    }
+
+    forStudent() {
+        return {id: this.id, weight: this.weight, name: this.name, max_score: this.max_score};
+    }
+
+    forInstructor() {
+        return {id: this.id, weight: this.weight, name: this.name, max_score: this.max_score};
+    }
+
+    forAdmin() {
+        return {id: this.id, weight: this.weight, name: this.name, max_score: this.max_score};
     }
 }
